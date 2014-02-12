51c69181e6c7a321abdde56ac1b5258ceccfcbef
Eric Huppert <eric@erichuppert.com>
2014-02-12 00:57:47 -0500

    latest on pID

--- addPrimaryIds.py
+++ addPrimaryIds.py
@@ -1,4 +1,18 @@
-for pitch in pitches:
-	gameSlug = None
-	atbat_number = None
+import MySQLdb
+verbose = True
+
+db = MySQLdb.connect("localhost", "baseball", "baseball1", "baseball")
+cursor = db.cursor()
+i = 0
+selectGameSQL = "SELECT id, slug FROM game LIMIT %i, 100" % str(i*100)
+cursor.execute(selectGameSQL)
+games = cursor.fetchall()
+
+for id, slug in games:
+	updateAtbatSQL = "UPDATE atbat SET game_id=%i where game_slug='%s'" % (id, slug)
+	cursor.execute(updateAtbatSQL)
+
+
 	
+
+
