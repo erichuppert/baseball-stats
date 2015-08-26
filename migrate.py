8964bb4705b1bacbb327e98db2188924a92f05d2
Eric Huppert <eric@erichuppert.com>
2015-08-25 18:12:43 -0700

    first set of changes, actually gonna make this look nice

--- migrate.py
+++ migrate.py
@@ -36,101 +36,31 @@ def migrate(directory, db):
 				date = datetime.datetime.strptime(date[:10], '%Y/%m/%d') 	
 			if gameAttribs['ampm'] == 'PM':
 				date += datetime.timedelta(hours=12)
-			date = date.strftime('%Y-%m-%d %H:%M:%S')
+			datetime = date.strftime('%Y-%m-%d %H:%M:%S')
 
-			day = gameAttribs.get("day", "null")
-			l = gameAttribs.get("league", "null")
-			gt = gameAttribs.get("game_type", "null")
-			hd = gameAttribs.get("home_division", "null")
-			ad = gameAttribs.get("away_division", "null")
-			gdsw = gameAttribs.get("gameday_sw", "null")
-			s = gameAttribs.get("status", "null")
-			ind = gameAttribs.get("ind", "null")
-			inn = gameAttribs.get("innings", "null") 
-			outs = gameAttribs.get("outs", "null")
-			ti = gameAttribs.get("top_inning", "null")
-			ac = gameAttribs.get("away_code", "null")
-			hc = gameAttribs.get("home_code", "null")
-			ati = gameAttribs.get("away_team_id", "null")
-			hti = gameAttribs.get("home_team_id", "null")
-			ana = gameAttribs.get("away_name_abbrev", "null")
-			hna = gameAttribs.get("home_name_abbrev", "null")
-			atc = gameAttribs.get("away_team_city", "null")
-			htc = gameAttribs.get("home_team_city", "null")
-			atr = gameAttribs.get("away_team_runs", "null")
-			htr = gameAttribs.get("home_team_runs", "null")
-			hw = gameAttribs.get("home_win", "null")
-			aw = gameAttribs.get("away_win", "null")
-			al = gameAttribs.get("away_loss", "null")
-			hl = gameAttribs.get("home_loss", "null")
-			ven = gameAttribs.get("venue", "null")
-			gpk = gameAttribs.get("game_pk", "null")
-			tz = gameAttribs.get("home_time_zone", "null")
-			atn = gameAttribs.get("away_team_name", "null")
-			ali = gameAttribs.get("away_league_id", "null")
-			htn = gameAttribs.get("home_team_name", "null")
-			hli = gameAttribs.get("home_league_id", "null")
-			agb = gameAttribs.get("away_games_back", "null")
-			if agb == '-':
-				agb = '0'
-			hgb = gameAttribs.get("home_games_back", "null")
-			if hgb == '-':
-				hgb = '0'
-			hgbw = gameAttribs.get("home_games_back_wildcard", "null")
-			if hgbw == '-':
-				hgbw = '0'
-			vwc = gameAttribs.get("venue_w_chan_loc", "null")
-			ath = gameAttribs.get("away_team_hits", "null")
-			hth = gameAttribs.get("home_team_hits", "null")
-			ate = gameAttribs.get("away_team_errors", "null")
-			hte = gameAttribs.get("home_team_errors", "null")
-			agbw = gameAttribs.get("away_games_back_wildcard", "null")
-			if agbw == '-':
-				agbw = '0'
-			reason = gameAttribs.get("reason", "null")
-			tvs = gameAttribs.get("tv_station", "null")
-			vid = gameAttribs.get("venue_id", "null")
-			des = gameAttribs.get("description", "null")
-			ser = gameAttribs.get("series", "null")
-			sern = gameAttribs.get("series_num", "null")
-			sg = gameAttribs.get("ser_games", "null")
+			# we will add the columns mapped to their corresponding value in this hash, then construct the SQL insert from that
+			gameAttributesHash = {datetime: datetime, id: id}
+			# these are all the values that we want from the gameAttribs hash
+			gameAttribKeys = ["id", "datetime", "day", "league", "game_type", "home_division", "away_division",
+				"gameday_sw", "status","ind", "inning", "outs", "top_inning", "away_team", "home_team",
+				"away_team_id", "home_team_id", "away_name_abbrev", "home_name_abbrev",
+				"away_team_city", "home_team_city", "away_team_runs", "home_team_runs", "home_win",
+				"home_loss", "away_win", "away_loss", "venue", "game_pk", "time_zone", "away_team_name",
+				"away_league_id", "home_team_name", "home_league_id",	"away_games_back", 
+				"home_games_back", "home_games_back_wildcard", "venue_w_chan_loc", "away_team_hits",
+				"home_team_hits", "away_team_errors", "home_team_errors", "away_games_back_wildcard",
+				"reason", "tv_station", "venue_id", "description", "series", "series_num", "ser_games"]
+			# we are basically filtering the full dictionary down to just the keys we want
+			for key in gameAttribKeys:
+				gameAttributesHash[key] = gameAttribs.get("day", "null")
 
-			gameSQL = """INSERT INTO game(id, datetime, day,
-			 	league, game_type, home_division, away_division, gameday_sw, status,
-				ind, inning, outs, top_inning, away_team, home_team, away_team_id, 
-				home_team_id, away_name_abbrev,
-				home_name_abbrev, away_team_city, home_team_city, away_team_runs,
-				home_team_runs, home_win, home_loss,
-				away_win, away_loss, venue, game_pk, time_zone, away_team_name,
-				away_league_id, home_team_name, home_league_id,
-				away_games_back, home_games_back,
-				home_games_back_wildcard, venue_w_chan_loc, away_team_hits,
-				home_team_hits, away_team_errors, home_team_errors, 
-				away_games_back_wildcard, reason, 
-				tv_station, venue_id, description, 
-				series, series_num, ser_games) 
-				VALUES ("{0}","{1}","{2}","{3}","{4}","{5}","{6}","{7}","{8}","{9}",
-					"{10}","{11}","{12}","{13}","{14}","{15}","{16}","{17}","{18}",
-					"{19}","{20}","{21}","{22}","{23}","{24}","{25}","{26}","{27}",
-					"{28}","{29}","{30}","{31}","{32}","{33}","{34}","{35}","{36}",
-					"{37}","{38}","{39}","{40}","{41}","{42}","{43}","{44}","{45}",
-					"{46}","{47}","{48}","{49}")""".format(id, date, day, l,\
-					gt, hd, ad, gdsw, s, ind, inn, outs, ti, ac, hc, ati, hti, ana,\
-					hna, atc, htc, atr, htr, hw, hl, aw, al, ven, gpk, tz, atn, ali,\
-					htn, hli, agb, hgb, hgbw, vwc, ath, hth, ate, hte, agbw,\
-					reason, tvs, vid, des, ser, sern, sg)
-
-			#mysql needs null without quotes, this simply removes the extra quotes
-			gameSQL = gameSQL.replace('"null"', "null")
-			if verbose:
-				print gameSQL
-			try:
-				cursor.execute(gameSQL)
-				db.commit()
-			except:
-				print gameSQL
-				db.rollback()
-			#end game attributes
+			# in the case when the team is in the lead, we want gb to be 0 rather than '-'
+			standingsKeys = ["away_games_back", "home_games_back", "home_games_back_wildcard", "away_games_back_wildcard"]
+			for key in standingsKeys:
+				if gameAttributesHash[key] == "-":
+					gameAttributesHash[key] = 0
+			
+			insertDict(gameAttributesHash, "game")
 
 		innings_all = ET.parse(root+'/innings_all.xml').getroot()
 		innings = innings_all.getchildren()
@@ -154,169 +84,64 @@ def migrate(directory, db):
 								print "atbat already in database"
 							pass
 						else:
-							b = atbatAttribs.get('b', 'null')
-							s = atbatAttribs.get('s', 'null')
-							o = atbatAttribs.get('o', 'null')
-							abstfsz = atbatAttribs.get('start_tfs_zulu', 'null')
-							if abstfsz != "" and abstfsz != 'null':
+							atbatKeys = ["b", "s", "o", "start_tfs_zulu", "batter", "stand", "b_height", "pitcher", "p_throws",
+								"des", "score", "home_team_runs", "away_team_runs", "event", "num"]
+							atbatDetailsForInsert = filterDict(atbatAttribs, atbatKeys)
+							abstfsz = atbatDetailsForInsert["start_tfs_zulu"]
+							if abstfsz not in ("", 'null'):
 								dt = datetime.datetime.strptime(abstfsz, '%Y-%m-%dT%H:%M:%SZ')
 								dt = dt.strftime('%Y-%m-%d %H:%M:%S')
 							else:
 								dt = 'null'
-							batter = atbatAttribs.get('batter', 'null')
-							stand = atbatAttribs.get('stand', 'null')
-							bheight = atbatAttribs.get('b_height', 'null')
-							pitcher = atbatAttribs.get('pitcher', 'null')
-							pthrow = atbatAttribs.get('p_throws', 'null')
-							des = atbatAttribs.get('des', 'null')
-							score = atbatAttribs.get('score', 'null')
-							htr = atbatAttribs.get('home_team_runs', 'null')
-							atr = atbatAttribs.get('away_team_runs', 'null')
-							event = atbatAttribs.get('event', 'null')
-
-							atbatSQL = """INSERT INTO atbat(game_id, num, b, s, o,
-								time, batter, stand, b_height,
-								pitcher, p_throws, des, score, home_team_runs,
-								away_team_runs, event, inning, inning_half) VALUES ("{0}",
-								"{1}","{2}","{3}","{4}","{5}","{6}","{7}","{8}","{9}",
-								"{10}","{11}","{12}","{13}","{14}","{15}","{16}","{17}")
-								""".format(id, abnum, b, s, o, dt,\
-								batter, stand, bheight, pitcher, pthrow, des, \
-								score, htr, atr, event, inning_num, inning_half)
-
-							atbatSQL = atbatSQL.replace('"null"', "null")
-
-							if verbose:
-								print atbatSQL
-
-							try:
-								cursor.execute(atbatSQL)
-								db.commit()
-							except:
-								print atbatSQL
-								db.rollback()
-								raise
+							atbatDetailsForInsert["start_tfs_zulu"] = dt
+							insertDict(atbatDetailsForInsert, "atbat")
 						events = ievent.getchildren()
 						balls = 0
 						strikes = 0
 						for event in events:
 							if event.tag == 'pitch':
-								pitchAttribs = event.attrib
-								pitchid = pitchAttribs['id']
+								allPitchAttribs = event.attrib
+								pitchid = allPitchAttribs['id']
 								checkSQL = "SELECT id FROM pitch WHERE game_id='%s' and atbat_num = '%s' and id = '%s'"%(id, abnum, pitchid)
 								cursor.execute(checkSQL)
 								if cursor.fetchone():
 									#print "pitch already in database"
 									pass
 								else:
-									des = pitchAttribs.get('des', 'null')
-									ptype = pitchAttribs.get('type', 'null')
-									abstfsz = pitchAttribs.get('tfs_zulu', 'null')
-									if abstfsz != "" and abstfsz != 'null':
+									pitchKeys = ["des", "type", "tfs_zulu", "x", "y", "mt", "on_1b", "on_2b", "on_3b", "sv_id", "start_speed",
+										"end_speed", "sz_top", "sz_bot", "pfx_x", "pfx_z", "px", "pz", "x0", "y0", "z0", "vx0", "vy0", "vz0",
+										"ax", "ay", "az", "break_y", "break_angle", "break_length", "pitch_type", "type_considence", "zone", "nasty",
+										"spin_dir", "spin_rate"]
+									pitchAttribs = filterDict(allPitchAttribs, pitchKeys)
+									abstfsz = pitchAttribs["tfs_zulu"]
+									if abstfsz not in ("", 'null'):
 										dt = datetime.datetime.strptime(abstfsz, '%Y-%m-%dT%H:%M:%SZ')
 										dt = dt.strftime('%Y-%m-%d %H:%M:%S')
 									else:
 										dt = 'null'
-									x = pitchAttribs.get('x', 'null')
-									y = pitchAttribs.get('y', 'null')
-									mt = pitchAttribs.get('mt', 'null')
-									if mt and verbose:
-										print 'mt not None for pitch', pitchid
-									first = pitchAttribs.get('on_1b', 'null')
-									second = pitchAttribs.get('on_2b', 'null')
-									third = pitchAttribs.get('on_3b', 'null')
-									svid = pitchAttribs.get('sv_id', 'null')
-									ss = pitchAttribs.get('start_speed', 'null')
-									es = pitchAttribs.get('end_speed', 'null')
-									sztop = pitchAttribs.get('sz_top', 'null')
-									szbot = pitchAttribs.get('sz_bot', 'null')
-									pfxx = pitchAttribs.get('pfx_x', 'null')
-									pfxz = pitchAttribs.get('pfx_z', 'null')
-									px = pitchAttribs.get('px', 'null')
-									pz = pitchAttribs.get('pz', 'null')
-									x0 = pitchAttribs.get('x0', 'null')
-									y0 = pitchAttribs.get('y0', 'null')
-									z0 = pitchAttribs.get('z0', 'null')
-									vx0 = pitchAttribs.get('vx0', 'null')
-									vy0 = pitchAttribs.get('vy0', 'null')
-									vz0 = pitchAttribs.get('vz0', 'null')
-									ax = pitchAttribs.get('ax', 'null')
-									ay = pitchAttribs.get('ay', 'null')
-									az = pitchAttribs.get('az', 'null')
-									breaky = pitchAttribs.get('break_y', 'null')
-									breakangle = pitchAttribs.get('break_angle', 'null')
-									breaklength = pitchAttribs.get('break_length', 'null') 
-									pitchtype = pitchAttribs.get('pitch_type', 'null')
-									type_confidence = pitchAttribs.get('type_confidence', 'null')
-									zone = pitchAttribs.get('zone', 'null')
-									nasty = pitchAttribs.get('nasty', 'null')
-									spindir = pitchAttribs.get('spin_dir', 'null')
-									spinrate = pitchAttribs.get('spin_rate', 'null')
-
-									
-
-									pitchSQL = """INSERT INTO pitch(des, id, type, time, x, y,
-										mt, on_1b, on_2b, on_3b, sv_id, start_speed, end_speed,
-										sz_top, sz_bot, pfx_x, pfx_z, px, pz, x0, y0, z0, vx0, vy0,
-										vz0, ax, ay, az, break_y, break_angle, break_length, pitch_type,
-										type_confidence, zone, nasty, spin_dir, spin_rate, balls, strikes,
-										atbat_num, game_id) VALUES("{0}","{1}","{2}","{3}","{4}","{5}",
-										"{6}","{7}","{8}","{9}","{10}","{11}","{12}","{13}","{14}","{15}",
-										"{16}","{17}","{18}","{19}","{20}","{21}","{22}","{23}","{24}",
-										"{25}","{26}","{27}","{28}","{29}","{30}","{31}","{32}","{33}",\
-										"{34}","{35}","{36}","{37}","{38}","{39}", "{40}")""".format(des, pitchid,\
-											ptype, dt, x, y, mt, first,	second, third, svid, ss, es, sztop,\
-											szbot, pfxx, pfxz, px, pz, x0, y0, z0, vx0, vy0, vz0, ax, ay,\
-											az, breaky, breakangle, breaklength, pitchtype, type_confidence,\
-											zone, nasty, spindir, spinrate, balls, strikes, abnum, id)
-									pitchSQL = pitchSQL.replace('"null"', "null")
-									try:
-										cursor.execute(pitchSQL)
-										db.commit()
-									except:
-										print pitchSQL
-										db.rollback()
-										raise
+									pitchAttribs["tfs_zulu"] = dt
+									insertDict(pitchAttribs, "pitch")
 
 									#update balls and strikes
-									if ptype in "SX" and strikes < 2:
+									if pitch["type"] in "SX" and strikes < 2:
 										strikes += 1
-									elif ptype == 'B':
+									elif pitch["type"] == 'B':
 										balls += 1
-									elif ptype not in "SXB":
-										print 'pitch', str(pitchid), 'from game', str(id), 'has type', str(ptype)
 							if event.tag == 'runner':
 								#do all of the runner attribute stuff
-								runnerAttribs = event.attrib
-								runnerid = runnerAttribs['id']
+								allRunnerAttribs = event.attrib
+								runnerid = allRunnerAttribs['id']
 								checkSQL = "SELECT id FROM runner WHERE game_id='%s' and atbat_num ='%s' and id='%s'"%(id, abnum, runnerid)
 								cursor.execute(checkSQL)
 								if cursor.fetchone():
 									"runner already in databse"
 								else:
-									start = runnerAttribs.get('start', 'null')
-									end = runnerAttribs.get('end', 'null')
-									event = runnerAttribs.get('event', 'null')
-									score = runnerAttribs.get('score', 'null')
-									rbi = runnerAttribs.get('rbi', 'null')
-									earned = runnerAttribs.get('earned', 'null')
-									runnerSQL = """INSERT INTO runner(id, start, end,
-										event, score, rbi, earned, game_id, atbat_num)
-										VALUES('{0}','{1}','{2}','{3}','{4}','{5}',
-										'{6}','{7}','{8}')""".format(runnerid, start,\
-										end, event, score, rbi, earned, id, abnum)
-									print runnerSQL
-
-									runnerSQL.replace("'null'", "null")
-									try:
-										cursor.execute(runnerSQL)
-										db.commit()
-									except:
-										db.rollback()
-										raise
+									runnerKeys = ["start", "end", "event", "score", "rbi", "earned"]
+									runnerAttribs = filterDict(allRunnerAttribs, runnerKeys)
+									insertDict(runnerAttribs, "runner")
 		
 		playerlist = ET.parse(root+'/players.xml').getroot()
-		teams = [t for t in playerlist.getchildren() if t.tag == 'team']
+		teams = filter(lambda x: x.tag == "team", playerlist.getchildren())
 		for team in teams:
 			playerteam = team.attrib['id']
 			players = [p for p in team.getchildren() if p.tag == 'player']
@@ -362,6 +187,24 @@ def migrate(directory, db):
 						db.rollback()
 						raise
 
+def insertDict(dict, tableName):
+	### This could be necessary, test it!
+	# #mysql needs null without quotes, this simply removes the extra quotes
+	# gameSQL = gameSQL.replace('"null"', "null")
+	qmarks = ', '.join('?' * len(myDict))
+	qry = "Insert into %s (%s) Values (%s)" % (tableName, qmarks, qmarks)
+	try:
+		cursor.execute(qry, myDict.keys() + myDict.values())
+		db.commit()
+	except:
+		db.rollback()
+		raise
+
+def filterDict(dict, keys):
+	result = {}
+	for key in keys:
+		result[key] = dict.get("day", "null")
+	return result
 
 if __name__ == "__main__":
 	db = MySQLdb.connect("localhost", "baseball", "baseball1", "baseball")
