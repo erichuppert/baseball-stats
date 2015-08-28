61a02826da5909669a8a6866c623d2cfd33443f1
Eric Huppert <eric@erichuppert.com>
2015-08-27 20:22:35 -0700

    renamed file, many more changes

--- game.py
+++ game.py
@@ -20,7 +20,7 @@ MLB_URL = "http://gd2.mlb.com/components/game/mlb"
 
 import urllib
 import xml.etree.ElementTree as ET
-import datetime
+from datetime import date
 import os
 import requests
 
@@ -33,18 +33,18 @@ class Game(object):
         id has the format gid_2015_08_24_oakmlb_seamlb_1
         """
         self.mlb_game_id = mlb_game_id
-        self.year = int(id[4:8])
-        self.month = int(id[9:11])
-        self.day = int(id[12:14])
         self.game_number = int(id[-1])
 
-        if datetime.datetime.now() < datetime.datetime(self.year, self.month,
-                                                       self.day):
+        year = int(id[4:8])
+        month = int(id[9:11])
+        day = int(id[12:14])
+        self.date = date(year, month, day)
+
+        # TODO: handle timezones more gracefully
+        if date.today() < self.date:
             raise Exception('Date of given game (' + mlb_game_id + ') is after current date')
 
-        uri_suffix = "year_{}/month_{}/day_{}/{}".format(
-            self.year, formattedDate(self.month), formattedDate(self.day), mlb_game_id
-        )
+        uri_suffix = "{}/{}".format(self.date.strftime("year_%Y/month_%m/day_%d"), mlb_game_id)
         self.local_dir = "{}/{}/month".format(DOWNLOAD_DIRECTORY, uri_suffix)
         self.base_url = "{}/{}".format(MLB_URL, uri_suffix)
 
@@ -71,6 +71,9 @@ class Game(object):
             os.makedirs(self.local_dir)
 
     def get_all_files(self):
+        """
+        Checks settings and downloads all the files that are needed
+        """
         if INNINGS_ALL:
             self.download_innings_all_file()
         if HIGHLIGHTS and self.year >= 2008:
@@ -92,9 +95,19 @@ class Game(object):
         if PLAYERS:
             self.getPlayers()
 
-    @staticmethod
-    def download_file(file_url, local_file_path, minimum_year=0):
+    def download_file(self, file_url, local_file_path, minimum_year=0):
+        """
+        given a the url of a file and a location to download the file to, downloads the file.
+        Optionally takes a minimum_year value, which will skip the download if self.year is
+        less than minimum_year. This optional value should be used when some file type was not
+        in existance for some year
+        """
+        if self.year < minimum_year:
+            return
         file_exists = os.path.isfile(local_file_path)
+        if file_exists or not OVERRIGHT_EXISTING_FILES:
+            return
+
         if not file_exists or OVERRIGHT_EXISTING_FILES:
             response = requests.get(file_url)
             response.raise_for_status()
@@ -105,128 +118,97 @@ class Game(object):
                         local_file.flush()
 
     def download_innings_all_file(self):
+        """
+        Downloads the 'innings_all' file, which contains pitch by pitch data, including PitchF/X
+        information. Before the year of 2008, these files don't exist for the entire game, but for
+        individual innings. This method handles 2007 gracefully by concatenating the files for each
+        inning so that the downloaded file always has the same format
+        """
         dest = self.local_dir + "/innings_all.xml"
         if not os.path.isfile(dest):
-            if self.year <= 2007:
-                outStr = "<game>"
+            if self.year < 2008:
+                full_xml = "<game>"
                 for i in range(self.innings):
                     inning_url = "{}/inning/innging{}.xml".format(self.base_url, str(i+1))
                     response = requests.get(inning_url)
                     response.raise_for_status()
-                    data = urllib.urlopen(inningURL).read()
-                    outStr += data
-                outStr += "</game>"
-                with open(dest, 'wb') as outFile:
-                    outFile.write(outStr)
+                    full_xml += response.text
+                full_xml += "</game>"
+                with open(dest, 'wb') as output_file:
+                    output_file.write(full_xml)
             else:
-                src = self.baseURL + "/inning/inning_all.xml"
-                this.download_file(src, dest)
+                src = self.base_url + "/inning/inning_all.xml"
+                self.download_file(src, dest)
 
-    def getHighlights(self):
+    def download_highlights_file(self):
+        """
+        Downloads the highlights file for the game, which contains information about video
+        highlights for a game.
+        """
         dest = self.local_dir + "/highlights.xml"
-        src = self.baseURL + "/media/highlights.xml"
+        src = self.base_url + "/media/highlights.xml"
         self.download_file(src, dest, minimum_year=2008)
 
 
-    def getGameEvents(self):
-        src = self.baseURL + "/game_events.xml"
+    def download_game_events_file(self):
+        """
+        Downloads the game events file, which contains play by play information about the game
+        """
+        src = self.base_url + "/game_events.xml"
         dest = self.local_dir + "/game_events.xml"
         self.download_file(src, dest, minimum_year=2008)
-        if not os.path.isfile(dest):
-            if self.year == 2007:
-                print "game_events.xml is not available for games before"
-            elif urllib.urlopen(src).getcode() == 404:
-                print "game_events.xml is not available for " + self.id
-            else:
-                urllib.urlretrieve(src, dest)
-        elif verbose:
-            "game_events.xml file for " + self.id + " is already downloaded"
 
-    def getLinescoreXML(self):
-        src = self.baseURL + "/linescore.xml"
+    def download_linescore_xml(self):
+        """
+        Downloads the linescore file for a game, which contains the basic overview of the game's
+        stats. Uses xml format
+        """
+        src = self.base_url + "/linescore.xml"
         dest = self.local_dir + "/linescore.xml"
-        if os.path.isfile(dest):
-            "linescore.xml for " + self.id + " is already downloaded"
-        elif urllib.urlopen(src).getcode() == 404:
-            print "linescore.xml is not available for " + self.id
-        else:
-            urllib.urlretrieve(src, dest)
+        self.download_file(src, dest)
 
-    def getPlayers(self):
-            src = self.baseURL + "/players.xml"
-            dest = self.localDir + "/players.xml"
-            if os.path.isfile(dest):
-                "linescore.xml for " + self.id + " is already downloaded"
-            elif urllib.urlopen(src).getcode() == 404:
-                print "linescore.xml is not available for " + self.id
-            else:
-                urllib.urlretrieve(src, dest)
+    def download_players_file(self):
+        """
+        downloads the players file for a given game, which gives a snapshot of the
+        """
+        src = self.base_url + "/players.xml"
+        dest = self.local_dir + "/players.xml"
+        self.download_file(src, dest)
 
 
-    def getLinescoreJSON(self):
-        src = self.baseURL + "/linescore.json"
-        dest = self.localDir + "/linescore.json"
-        if not os.path.isfile(dest):
-            if self.year <= 2007:
-                print "linescore.json is not available for games before 2008"
-            elif urllib.urlopen(src).getcode() == 404:
-                print "linescore.json is not available for " + self.id
-            else:
-                urllib.urlretrieve(src, dest)
-        elif verbose:
-            "linescore.json for " + self.id + " is already downloaded"
+    def download_linescore_json(self):
+        """
+        Downloads the linescore file for a game, which contains the basic overview of the game's
+        stats. Uses json format
+        """
+        src = self.base_url + "/linescore.json"
+        dest = self.local_dir + "/linescore.json"
+        self.download_file(src, dest, minimum_year=2008)
 
-    def getBoxscoreXML(self):
-        src = self.baseURL + "/boxscore.xml"
-        dest = self.localDir + "/boxscore.xml"
-        if os.path.isfile(dest):
-            "boxscore.xml for " + self.id + " is already downloaded"
-        elif urllib.urlopen(src).getcode() == 404:
-            print "boxscore.xml is not available for " + self.id
-        else:
-            urllib.urlretrieve(src, dest)
+    def download_boxscore_xml(self):
+        """
+        Downloads the boxscore file in xml format
+        """
+        src = self.base_url + "/boxscore.xml"
+        dest = self.local_dir + "/boxscore.xml"
+        self.download_file(src, dest)
 
-    def getBoxscoreJSON(self):
-        src = self.baseURL + "/boxscore.json"
-        dest = self.localDir + "/boxscore.json"
-        if not os.path.isfile(dest):
-            if self.year <= 2011:
-                print "linescore.json is not available for games before 2012"
-            else:
-                urllib.urlretrieve(src, dest)
-        elif verbose:
-            "boxscore.json for " + self.id + " is already downloaded"
+    def download_boxscore_json(self):
+        """
+        Downloads the boxscore file in xml format
+        """
+        src = self.base_url + "/boxscore.json"
+        dest = self.local_dir + "/boxscore.json"
+        self.download_file(src, dest)
 
-    def getEventLog(self):
-        src = self.baseURL + "/eventLog.xml"
-        dest = self.localDir + "/eventLog.xml"
-        if not os.path.isfile(dest):
-            if self.year <= 2006:
-                print "linescore.json is not available for games before 2007"
-            else:
-                urllib.urlretrieve(src, dest)
-        elif verbose:
-            print "eventLog.xml for " + self.id + " is already downloaded"
+    def download_raw_boxscore(self):
+        """
+        Downloads the raw boxscore file
+        """
+        src = self.base_url + "/rawboxscore.xml"
+        dest = self.local_dir + "/rawboxscore.xml"
+        self.download_file(src, dest, minimum_year=2012)
 
-    def getRawBoxscore(self):
-        src = self.baseURL + "/rawboxscore.xml"
-        dest = self.localDir + "/rawboxscore.xml"
-        if not os.path.isfile(dest):
-            if self.year <= 2011:
-                print "rawboxscore.xml is not available for games before 2012"
-            elif urllib.urlopen(src).getcode() == 404:
-                print "rawboxscore.xml is not available for " + self.id
-            else:
-                urllib.urlretrieve(src, dest)
-        elif verbose:
-            print "rawboxscore.xml for " + self.id + " is already downloaded"
-
-#returns a properly formatted string for date (adds a leading zero to single digit numbers)
-def formattedDate(number):
-    if number < 10:
-        return "0" + str(number)
-    else:
-        return str(number)
 
 #returns a list of all game objects for a given day
 def getGames(year, month, day):
@@ -283,31 +265,5 @@ def update(start = datetime.date(2006,3,23), end = datetime.date.today()):
             currentDate = datetime.date(currentDate.year-1, 11, 5)
         else:
             currentDate -= datetime.timedelta(days=1)
-
-#check to see if all desired for a list of games has been downloaded. Returns a boolean value.
-def hasAllFiles(games):
-    for game in games:
-        if INNINGS_ALL and not(os.path.isfile(game.localDir + "/innings_all.xml")):
-            return False
-        if HIGHLIGHTS and not(os.path.isfile(game.localDir + "/highlights.xml")):
-            return False
-        if GAME_EVENTS and not(os.path.isfile(game.localDir + "/game_events.xml")):
-            return False
-        if LINESCORE_XML and not(os.path.isfile(game.localDir + "/linescore.xml")):
-            return False
-        if LINESCORE_JSON and game.year >= 2007 and not(os.path.isfile(game.localDir + "/linescore.json")):
-            return False
-        if BOX_SCORE_XML and not(os.path.isfile(game.localDir + "/boxscore.xml")):
-            return False
-        if BOX_SCORE_JSON and game.year >= 2013 and not (os.path.isfile(game.localDir + "/boxscore.json")):
-            return False
-        if EVENT_LOG and game.year >= 2007 and not (os.path.isfile(game.localDir + "/eventLog.xml")):
-            return False
-        if RAW_BOXSCORE and game.year >= 2011 and not (os.path.isfile(game.localDir + "/rawboxscore.xml")):
-            return False
-        if players and not (os.path.isfile(game.localDir + "/players.xml")):
-            return False
-        return True
-
 
 #update(end = datetime.date(2006,7,7))
